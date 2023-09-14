2c7a4864b621c55f6da6756818f73e326761d018
Boe Reh <justboereh@gmail.com>
2023-09-14 18:01:34 -0500

    first commit

---
+++ src/lib/title/component.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    export let level: number = 1
+</script>
+
+
+<svelte:element this={`h${level || 1}`} class="ant-typography">
+    <slot />
+</svelte:element>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+    import Text from "../lib/text/component.svelte";
+    import Title from "../lib/title/component.svelte";
+
+    import "../lib/light.css";
+</script>
+
+<div>
+    <Text>Ant Design (default)</Text>
+    <Text type="secondary">Ant Design (secondary)</Text>
+    <Text type="success">Ant Design (success)</Text>
+    <Text type="warning">Ant Design (warning)</Text>
+    <Text type="danger">Ant Design (danger)</Text>
+    <Text disabled>Ant Design (disabled)</Text>
+    <Text mark>Ant Design (mark)</Text>
+    <Text code>Ant Design (code)</Text>
+    <Text keyboard>Ant Design (keyboard)</Text>
+    <Text underline>Ant Design (underline)</Text>
+    <Text delete>Ant Design (delete)</Text>
+    <Text strong>Ant Design (strong)</Text>
+    <Text italic>Ant Design (italic)</Text>
+</div>
+
+
+<div>
+    <Title>h1. Ant Design</Title>
+    <Title level={2}>h2. Ant Design</Title>
+    <Title level={3}>h3. Ant Design</Title>
+    <Title level={4}>h4. Ant Design</Title>
+    <Title level={5}>h5. Ant Design</Title>
+</div>
+
+<style>
+    div {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+    }
+</style>
